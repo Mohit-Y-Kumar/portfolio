827697d2e1817bbe21f35cad5b2112533a8996c2
Mohit-Y-Kumar <my1362321@gmail.com>
2025-07-02 21:58:43 +0530

    update images

--- dashboard/src/pages/Login.jsx
+++ dashboard/src/pages/Login.jsx
@@ -86,7 +86,7 @@ const Login = () => {
         </div>
       </div>
       <div className="flex justify-center items-center bg-muted h-[400px] w-[450px]">
-        <img src="/login.jpeg" alt="login" />
+        <img src="/lgn.avif" alt="login" />
       </div>
     </div>
   );
